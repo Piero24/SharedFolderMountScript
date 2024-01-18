82398fc84b275735fbff0779f813b3f5c122c513
Blake Barnhill <blakeb21@vt.edu>
2024-01-18 11:44:14 -0500

    potential changes

--- mounter.sh
+++ mounter.sh
@@ -24,33 +24,34 @@ moviesMount=0
 seriesMount=0
 
 # Loop to attempt editing at 30 second intervals
+# Replace the objects in brackets with the appropriate location of your NAS and the path you would like to mount the folder
 while true; do
 
     if [ $downMount -eq 0 ]; then
-        sudo mount -t cifs //IP-ADDRESS/PATH /PATH-TO-MOUNT /DATA/Downloads -o username=USERNAME-HERE,password=PASSWORD-HERE,vers=3.0,rw,uid=1000,gid=1000,forceuid,forcegid,file_mode=0777,dir_mode=0777,nounix
+        sudo mount -t cifs [//IP-ADDRESS/PATH] [/PATH-TO-MOUNT default: /DATA/Downloads] -o username=USERNAME-HERE,password=PASSWORD-HERE,vers=3.0,rw,uid=1000,gid=1000,forceuid,forcegid,file_mode=0777,dir_mode=0777,nounix
 
         if [ $? -eq 0 ]; then
-            echo "mount successful!"
+            echo "downloads mount successful!"
             downMount=1
         fi
     
     fi
 
     if [ $moviesMount -eq 0 ]; then
-        sudo mount -t cifs //IP-ADDRESS/PATH /PATH-TO-MOUNT /DATA/Media/Movies -o username=USERNAME-HERE,password=PASSWORD-HERE,vers=3.0,rw,uid=1000,gid=1000,forceuid,forcegid,file_mode=0777,dir_mode=0777,nounix
+        sudo mount -t cifs [//IP-ADDRESS/PATH] [/PATH-TO-MOUNT default: /DATA/Media/Movies] -o username=USERNAME-HERE,password=PASSWORD-HERE,vers=3.0,rw,uid=1000,gid=1000,forceuid,forcegid,file_mode=0777,dir_mode=0777,nounix
 
         if [ $? -eq 0 ]; then
-            echo "mount successful!"
+            echo "movies mount successful!"
             moviesMount=1
         fi
     
     fi
 
     if [ $seriesMount -eq 0 ]; then
-        sudo mount -t cifs //IP-ADDRESS/PATH /PATH-TO-MOUNT /DATA/Media/TV\ Shows -o username=USERNAME-HERE,password=PASSWORD-HERE,vers=3.0,rw,uid=1000,gid=1000,forceuid,forcegid,file_mode=0777,dir_mode=0777,nounix
+        sudo mount -t cifs [//IP-ADDRESS/PATH] [/PATH-TO-MOUNT default: /DATA/Media/TV\ Shows] -o username=USERNAME-HERE,password=PASSWORD-HERE,vers=3.0,rw,uid=1000,gid=1000,forceuid,forcegid,file_mode=0777,dir_mode=0777,nounix
 
         if [ $? -eq 0 ]; then
-            echo "mount successful!"
+            echo "shows mount successful!"
             seriesMount=1
         fi
     
@@ -63,6 +64,7 @@ while true; do
     fi
     
     # Please wait 30 seconds before trying again
+    echo "An error has occurred, retrying in 30secs"
     sleep 30
 done
 echo "Done!"
